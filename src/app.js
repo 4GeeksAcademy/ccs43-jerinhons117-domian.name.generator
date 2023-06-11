e54cfcb3cc39c87f48fbdc3b1385ca27955d9194
jerinhons117 <131413483+jerinhons117@users.noreply.github.com>
2023-06-11 16:37:34 +0000

    proyecto de domain Name generator terminado

--- src/app.js
+++ src/app.js
@@ -1,11 +1,31 @@
 /* eslint-disable */
-import "bootstrap";
+/*import "bootstrap";
 import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+import "./assets/img/4geeks.ico";*/
 
-window.onload = function() {
+/*window.onload = function() {
   //write your code here
+
   console.log("Hello Rigo from the console!");
-};
+};*/
+
+const pronoun = ["the", "our"];
+const adj = ["great", "big"];
+const noun = ["jogger", "racoon"];
+const ext = [".com", ".net", ".ve", ".go"];
+
+function domainName(pronouns, adjetives, nouns, exts) {
+  for (let pronoun of pronouns) {
+    for (let adj of adjetives) {
+      for (let noun of nouns) {
+        for (let ext of exts) {
+          console.log(pronoun + adj + noun + ext);
+        }
+      }
+    }
+  }
+}
+
+domainName(pronoun, adj, noun, ext);
